118c6f585ebde862632fd9368b1936e08bce8deb
Sarthak <sarthak.00puri@gmail.com>
2024-11-27 23:20:59 +0530

    STYLE: added templates for blog page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+posts=[
+    {
+        'author':"Sarthak",
+        'title':"Blog Post 1",
+        'content':"First post content",
+        'date_posted':"November 27, 2024"
+    },
+    {
+        'author':"Manan",
+        'title':"Blog Post 2",
+        'content':"Second post content",
+        'date_posted':"November 27, 2024"
+    }
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
