174081b0b574415e601fe24f07db7a795ea67bcc
Sarthak <sarthak.00puri@gmail.com>
2024-12-03 09:15:29 +0530

    Feat: Added post model to store and display blog post data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts=[
-    {
-        'author':"Sarthak",
-        'title':"Blog Post 1",
-        'content':"First post content",
-        'date_posted':"November 27, 2024"
-    },
-    {
-        'author':"Manan",
-        'title':"Blog Post 2",
-        'content':"Second post content",
-        'date_posted':"November 27, 2024"
-    }
-]
+# posts=[
+#     {
+#         'author':"Sarthak",
+#         'title':"Blog Post 1",
+#         'content':"First post content",
+#         'date_posted':"November 27, 2024"
+#     },
+#     {
+#         'author':"Manan",
+#         'title':"Blog Post 2",
+#         'content':"Second post content",
+#         'date_posted':"November 27, 2024"
+#     }
+# ]
 def home(request):
+    
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
