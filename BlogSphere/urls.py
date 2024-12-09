532fe64a2b7562917bce4fe59363502810a28af1
Sarthak <sarthak.00puri@gmail.com>
2024-12-10 01:46:24 +0530

    FEAT: added login mechanism

--- BlogSphere/urls.py
+++ BlogSphere/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from user import views as userViews
-
+from django.contrib.auth import views as authViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
-    path('register/',userViews.register,name='user-register')
+    path('register/',userViews.register,name='user-register'),
+    path('login/',authViews.LoginView.as_view(template_name='user/login.html'),name='user-login'),
+    path('logout/',authViews.LogoutView.as_view(),name='user-logout'),
 ]
