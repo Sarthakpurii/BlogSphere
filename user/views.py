b55c0deb6c419ce1e8b09b8bef4e8749227e2dc2
Sarthak <sarthak.00puri@gmail.com>
2024-12-09 08:59:44 +0530

    STYLE: updated register template powered with crispy-forms & bootstrap

--- user/views.py
+++ user/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 from django.contrib import messages
 
 def register(request):
     if request.method=='POST':
-        form=UserCreationForm(request.POST)
+        form=UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request,f"Congrats! Account has been created for {username}.")
             return redirect('blog-home')
     else:
-        form=UserCreationForm()
+        form=UserRegistrationForm()
     return render(request,template_name='user/register.html',context={'form':form})
 
