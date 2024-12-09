79ba5c1b951fc6458ef70e75aa82c46ce6a74f2b
Sarthak <sarthak.00puri@gmail.com>
2024-12-09 06:36:31 +0530

    FEAT: Added user registration form

--- BlogSphere/urls.py
+++ BlogSphere/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from user import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls'))
+    path('blog/',include('blog.urls')),
+    path('register/',userViews.register,name='user-register')
 ]
