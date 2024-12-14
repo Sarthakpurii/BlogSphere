b830a34c407d2113dda3a3deee990eb8b150882b
Sarthak <sarthak.00puri@gmail.com>
2024-12-14 23:39:33 +0530

    FEAT: updated profile form

--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+
 def register(request):
     if request.method=='POST':
         form=UserRegistrationForm(request.POST)
@@ -14,6 +16,31 @@ def register(request):
         form=UserRegistrationForm()
     return render(request,template_name='user/register.html',context={'form':form})
 
+
 @login_required
 def profile(request):
-    return render(request,template_name='user/profile.html')
+    if request.method=='POST':
+        user_instance = request.user
+        uForm=UserUpdateForm(request.POST,instance=user_instance)
+        pForm=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if uForm.is_valid() and pForm.is_valid():
+            uForm.save()
+            pForm.save()
+            messages.success(request,f"Your Profile has been Updated.")
+            return redirect('user-profile')
+            
+    else:
+        uForm=UserUpdateForm(instance=request.user)
+        pForm=ProfileUpdateForm(instance=request.user.profile)
+    request.user.refresh_from_db()
+    print("Request Method:", request.method)
+    print("User Form Data:", uForm.data if request.method == 'POST' else "N/A")
+    print("Is User Form Valid?", uForm.is_valid())
+    print("Errors in User Form:", uForm.errors if not uForm.is_valid() else "No Errors")
+    print("Current User Username:", request.user.username)
+    context={
+        'uForm':uForm,
+        'pForm':pForm,
+        'user_data': request.user,
+    }
+    return render(request,template_name='user/profile.html', context=context)
