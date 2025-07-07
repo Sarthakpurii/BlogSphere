a06183a4e67dbf1f53ba5adc9df2089521ed2b14
Sarthak <sarthak.00puri@gmail.com>
2025-07-07 16:47:14 +0530

    FIX: updated max title length for blogs

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title=models.CharField(max_length=70,blank=False,null=False)
+    title=models.CharField(max_length=30,blank=False,null=False)
     content=models.TextField()
     date_posted=models.DateTimeField(auto_now_add=True)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
